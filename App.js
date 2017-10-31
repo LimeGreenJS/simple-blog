4361709ce33bf67bae54dc54ff3f268420eade7f
Daishi Kato <dai-shi@users.noreply.github.com>
2017-10-31 22:03:44 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
 import ArticleList from './ArticleList';
@@ -8,7 +9,8 @@ import ArticleList from './ArticleList';
 const link = new HttpLink({
   uri: 'https://api.graphcms.com/simple/v1/cj98b2tds1fha0115h7csyj5d',
 });
-const client = new ApolloClient({ link });
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
 
 const App = () => (
   <ApolloProvider client={client}>
